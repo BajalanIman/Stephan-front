0f90c8185ccfe37d33c5ccfb64d4f5a488659cd0
Iman <bajalaniman@gmail.com>
2025-03-03 17:44:21 +0100

    first commit

--- components/login/SingUp.jsx
+++ components/login/SingUp.jsx
@@ -65,7 +65,9 @@ const SingUp = () => {
       last_login_at: "2024-08-26T22:00:00.000Z",
     };
     axios
-      .post(`${BASE_URL}users`, data, { withCredentials: true })
+      .post("https://stephan-backend-production.up.railway.app/users", data, {
+        withCredentials: true,
+      })
       .then((res) => {
         localStorage.setItem("userId", res.data.user_id);
         if (res.data.message === "Record inserted successfully") {
